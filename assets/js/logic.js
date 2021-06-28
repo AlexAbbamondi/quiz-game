a3388344e513742da738103e376c3c733d6c3789
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-06-28 19:05:16 -0400

    added functionality for reponse for correct/incorrect answer, final score with counter, and storing local data in the form of the count and the users initials

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,2 +1,123 @@
-var startbtn = document.querySelector("#start");
-var questionsEl = document.querySelector("")
+var time = questions.length * 15;
+var currQuestionIndex = 0;
+var count = 0;
+
+var startBtn = document.querySelector("#start");
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var questionChoices = document.querySelector("#choices");
+var startScreen = document.querySelector("#start-screen");
+var titleEl = document.querySelector("#question-title");
+var endScreen = document.querySelector("#end-screen");
+var score = document.getElementById("final-score");
+var submitBtn = document.getElementById("submit");
+
+var initials = document.getElementById("initials");
+var storedInitials = localStorage.getItem("initial");
+
+var counter = document.getElementById("counter");
+var storedCount = localStorage.getItem("count");
+
+function startQuiz() {
+  startScreen.setAttribute("class", "hide");
+  questionsEl.removeAttribute("class", "hide");
+  getCurrentQuestion();
+  timer();
+}
+
+//Gets the current question, displys it to the pages with the fours answer options
+function getCurrentQuestion() {
+  var currentQuestion = questions[currQuestionIndex];
+  titleEl.textContent = currentQuestion.title;
+  questionChoices.textContent = "";
+
+  //creates a button --- set current question choice --- append choices to page
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", currentQuestion.choices[i]);
+
+    choiceNode.textContent = i + 1 + ". " + currentQuestion.choices[i];
+
+    questionChoices.appendChild(choiceNode);
+
+    //check to see if the right answer was chosen and respond accordingly --- show end screen after the 5th questions
+    var answer = questions[currQuestionIndex].answer;
+
+    choiceNode.addEventListener("click", function (event) {
+      if (event.target.getAttribute("value") === answer) {
+        correctAnswer();
+        currQuestionIndex++;
+        count++;
+        if (
+          currQuestionIndex === currentQuestion.choices.length + 1 &&
+          event.target.getAttribute("value") === answer
+        ) {
+          endScreen.removeAttribute("class", "hide");
+          questionsEl.setAttribute("class", "hide");
+          finalScore();
+        } else {
+          getCurrentQuestion();
+        }
+      } else {
+        wrongAnswer();
+        currQuestionIndex++;
+        if (
+          currQuestionIndex === currentQuestion.choices.length + 1 &&
+          event.target.getAttribute("value") != answer
+        ) {
+          endScreen.removeAttribute("class", "hide");
+          questionsEl.setAttribute("class", "hide");
+          finalScore();
+        } else {
+          getCurrentQuestion();
+        }
+      }
+    });
+  }
+}
+
+//Create response for correct answer
+function correctAnswer() {
+  var correctAnswer = document.createElement("h3");
+  correctAnswer.setAttribute("class", "responses");
+  correctAnswer.textContent = "correct";
+  document.querySelector("#responses").appendChild(correctAnswer);
+}
+
+//Create response for incorrect answer
+function wrongAnswer() {
+  var wrongAnswer = document.createElement("h3");
+  wrongAnswer.setAttribute("class", "responses");
+  wrongAnswer.textContent = "Incorrect";
+  document.querySelector("#responses").appendChild(wrongAnswer);
+}
+
+//timer function --- decrements every second
+function timer() {
+  var timer = setInterval(function () {
+    if (time <= 0) {
+      clearInterval(timer);
+      timerEl.innerHTML = 0;
+    } else {
+      timerEl.innerHTML = time;
+    }
+    time -= 1;
+  }, 1000);
+}
+
+//shows final score to user and sets its to a key value pair
+function finalScore() {
+  score.textContent = count;
+  localStorage.setItem("count", count);
+}
+
+//on submit sets the initials to a key value pair
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  localStorage.setItem("initial", initials.value);
+})
+
+
+//create li with initials and count
+startBtn.addEventListener("click", startQuiz);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,27 @@
 var questions = [
     {
-        title:
-        choices: []
-        answer:
+        title: "Which of the following is an advantage of using JavaScript?",
+        choices: ["Less server interaction", "Immediate feedback to the visitors", "Increased interactivity", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        title: "How can you get the type of arguments passed to a function?",
+        choices: ["Using typeof operator", "Using getType function", "Both of the above", "None of the above"],
+        answer: "Using typeof operator"
+    },
+    {
+        title: "Which built-in method combines the text of two strings and returns a new string?",
+        choices: ["append()", "concat()", "attach()", "None of the above"],
+        answer: "concat()"
+    },
+    {
+        title: "Which built-in method sorts the elements of an array?",
+        choices: ["changeOrder(order)", "order()", "sort()", "None of the above"],
+        answer: "sort()"
+    },
+    {
+        title: "Which of the following function of String object returns a number indicating the Unicode value of the character at the given index?",
+        choices: ["charAt()", "charCodeAt()", "concat()", "indexOf()"],
+        answer: "charCodeAt()"
     }
 ]
